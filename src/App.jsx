b1b1a8dd040a2a462e584cfd8281eda777067420
Yusufcan Var <yusufcanvar90@gmail.com>
2025-03-23 18:11:00 +0300

    Removed lazy loading from the hero.webp to preven the LCP being lazy loaded

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ function App() {
       <div className="pattern" />
       <div className="wrapper">
         <header>
-          <img src="./hero.webp" alt="hero banner" loading="lazy" />
+          <img src="./hero.webp" alt="hero banner" />
           <h1>
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without Hassle
